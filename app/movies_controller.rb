c6299b58835fdf34fa1c696b53a7e9f0011c332c
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-23 06:14:31 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,11 +22,11 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.new(attributes)
 end
 
-#def can_be_created_in_a_block(args = {__})
-#  Movie.create do |m|
-#    __
-#  end
-#end
+def can_be_created_in_a_block(args = {__})
+  Movie.create do |m|
+    __
+  end
+end
 
 def can_get_the_first_item_in_the_database
   movie = Movie.first
