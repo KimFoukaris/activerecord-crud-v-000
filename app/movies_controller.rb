189cd067d7907e5f0f0f1a1b559cd78a4d83280d
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-23 06:32:39 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    movie.title = title: "Home Alone"
+    movie.title = "Home Alone"
     movie.release_date = release_date: 1990
   end
     binding.pry
